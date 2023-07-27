84314a123cb365dd1cba9049b5d6ccfbf83adb74
Diogo Almeida <diogo.g07@icloud.com>
2023-07-27 14:12:54 -0300

    atualizacao

--- internal/graph/storage/product.go
+++ internal/graph/storage/product.go
@@ -9,6 +9,8 @@ import (
 
 type contextKey string
 
+// https://gqlgen.com/reference/dataloaders/
+// https://w11i.me/graphql-server-go-part2-dataloaders
 const (
 	loaders = contextKey("products-loaders")
 )
