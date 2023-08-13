6c0784f4d424189d8bb5de065e9e80ca90309413
diogoX451 <diogo.g07@icloud.com>
2023-08-13 20:45:47 -0300

    melhoria

---
+++ internal/factory/send_email.go
@@ -0,0 +1,13 @@
+package factory
+
+type ISendEmail interface {
+	MultiEmail(templateID string, filter interface{}) (string, error)
+	SendOneEmail(userID string, templateID, filter interface{}) (string, error)
+	SendEmail(templateID string, filter interface{}) (string, error)
+}
+
+func MultiEmail(filter interface{}) (ISendEmail, error) {
+
+}
+
+func SendOneEmail(userID string, templateID string, filter interface{}) (string, error) {}
